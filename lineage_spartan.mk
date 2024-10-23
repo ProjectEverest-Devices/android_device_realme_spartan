858be12c1f1f398976eca19fe60514bbef4e8625
LuK1337 <priv.luk@gmail.com>
2024-10-23 22:11:50 +0800

    spartan: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
    Signed-off-by: ryukftw <workforbijoy9@gmail.com>

--- lineage_spartan.mk
+++ lineage_spartan.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,15 +20,13 @@ PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3371
 
-PRODUCT_SYSTEM_NAME := RMX3371
-PRODUCT_SYSTEM_DEVICE := RE54E4L1
-
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="RMX3371-user 14 UKQ1.230924.001 1725460632695 release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := realme/RMX3371/RE54E4L1:14/UKQ1.230924.001/1725460632695:user/release-keys
+    BuildDesc="RMX3371-user 14 UKQ1.230924.001 1725460632695 release-keys" \
+    BuildFingerprint=realme/RMX3371/RE54E4L1:14/UKQ1.230924.001/1725460632695:user/release-keys
+    DeviceName=RMX3371 \
+    DeviceProduct=RMX3371 \
+    SystemDevice=RE54E4L1 \
+    SystemName=RMX3371
 
